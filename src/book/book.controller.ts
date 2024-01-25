39b0ee2cae8eb682a442472a6dd6cb4ee3584cf0
Shudhanshu-Mckinley <shudhanshu.abhinav@mckinleyrice.co>
2024-01-25 18:17:13 +0530

    Initial commit

---
+++ src/book/book.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
+import { createBookDto } from './dto/create-book dto';
+
+@Controller('books')
+export class BookController {
+    constructor(private bookService: BookService) {}
+
+    @Get()
+    async getAllBooks(): Promise<Book[]> {
+        return this.bookService.findAll()  
+    }
+    
+    @Post()
+    async createBook(
+        @Body()
+        book: createBookDto,
+    ): Promise<Book> {
+        return this.bookService.create(book)
+    }
+
+    @Get(':id')
+    async getBooks(
+        @Param('id')
+        id: string,
+    ): Promise<Book> {
+        return this.bookService.findById(id);  
+    }
+}
